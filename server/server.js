6c12e1322acb7f141a7570f691d822ea0aed6f5c
Null McNull <no@spam.org>
2013-10-06 20:50:22 +0200

    Disable cache for IE

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ var app = express();
 
 app.use(express.logger());                                  // Log requests to the console
 app.use(express.bodyParser());                              // Extract the data from the body of the request
+app.use(function noCache(req, res, next){
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires",0);
+  next();
+});
 
 require('./lib/routes/quotes').addRoutes(app, config);
 require('./lib/routes/forms').addRoutes(app, config);
