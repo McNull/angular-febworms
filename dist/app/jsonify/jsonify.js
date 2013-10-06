344dfe54a359272b1eda874e98983ea38af0b7fc
Null McNull <no@spam.org>
2013-10-06 16:29:47 +0200

    Grunt debug refactor

---
+++ dist/app/jsonify/jsonify.js
@@ -0,0 +1,10 @@
+angular.module('myApp').directive('jsonify', function() {
+  return {
+    templateUrl: 'app/jsonify/jsonify.tmpl.html',
+    restrict: 'A',
+    replace: true,
+    scope: {
+      jsonify: "="
+    }
+  };
+});
