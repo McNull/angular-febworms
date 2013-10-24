adebb63f963682294870f8b2334c03a9ebb3fcea
Null McNull <no@spam.org>
2013-10-24 09:43:48 +0200

    febwormsUniqueFieldName

--- src/febworms/edit/canvas/febworms-canvas.js
+++ src/febworms/edit/canvas/febworms-canvas.js
@@ -1,4 +1,4 @@
-angular.module('febworms').directive('febwormsCanvas',function (febwormsUtils) {
+angular.module('febworms').directive('febwormsCanvas',function () {
 
   return {
     templateUrl: 'febworms/edit/canvas/febworms-canvas.tmpl.html',
@@ -20,23 +20,23 @@ angular.module('febworms').directive('febwormsCanvas',function (febwormsUtils) {
       $scope.dragPlaceholder.visible = false;
     };
 
-    $scope.dragBeginCanvasField = function(index, field) {
+    $scope.dragBeginCanvasField = function (index, field) {
 
       // Delay is set to prevent browser from copying adjusted html as copy image
 
-      $timeout(function() {
+      $timeout(function () {
         field.$_isDragging = true;
       }, 1);
 
       return { source: 'canvas', field: field, index: index };
     };
 
-    $scope.dragEndCanvasField = function(field) {
+    $scope.dragEndCanvasField = function (field) {
 
       // IE Fix: ensure this is fired after the drag begin
 
-      $timeout(function() {
-      field.$_isDragging = false;
+      $timeout(function () {
+        field.$_isDragging = false;
       }, 10);
     };
 
@@ -44,14 +44,14 @@ angular.module('febworms').directive('febwormsCanvas',function (febwormsUtils) {
 
       var dragData = dqUtils.dragData();
 
-      if(dragData && dragData.data) {
+      if (dragData && dragData.data) {
 
         var field = dragData.data.field;
         var source = dragData.data.source;
         var index = dragData.data.index;
         var fields = $scope.schema.fields;
 
-        if(source == 'palette') {
+        if (source == 'palette') {
           field = $scope.copyPaletteField(field);
           fields.splice($scope.dragPlaceholder.index, 0, field);
         } else if (source == 'canvas') {
@@ -80,16 +80,52 @@ angular.module('febworms').directive('febwormsCanvas',function (febwormsUtils) {
         formField: '='
       }
     }
-  }).directive('febwormsCanvasFieldProperties', function () {
+  }).directive('febwormsCanvasFieldProperties',function () {
     return {
       templateUrl: 'febworms/edit/canvas/febworms-canvas-field-properties.tmpl.html',
       replace: true,
       scope: {
-        field: '='
+        field: '=',
+        schema: '='
       },
-      link: function (scope) {
-        scope.$watch('fieldPropertiesForm.$invalid', function (newValue) {
-          scope.field.$_invalid = newValue;
+      link: function ($scope) {
+        $scope.$watch('fieldPropertiesForm.$invalid', function (newValue) {
+          $scope.field.$_invalid = newValue;
+        });
+      }
+    };
+  }).directive('febwormsUniqueFieldName', function () {
+
+    var changeTick = 0;
+
+    function validate(ngModel, field, fields) {
+      var valid = true;
+      var schemaField;
+
+      for (var i = 0; i < fields.length; i++) {
+        schemaField = fields[i];
+        if (schemaField !== field && field.name === schemaField.name) {
+          valid = false;
+          break;
+        }
+      }
+
+      ngModel.$setValidity('unique', valid);
+    }
+
+    return {
+      require: 'ngModel',
+      link: function ($scope, $element, $attrs, ngModel) {
+
+        var field = $scope.field;
+        var fields = $scope.schema.fields;
+
+        $scope.$watch('field.name', function () {
+          ++changeTick;
+        });
+
+        $scope.$watch(function() { return changeTick; }, function() {
+          validate(ngModel, field, fields);
         });
       }
     };
