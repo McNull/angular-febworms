e95224be86d2feae2521f83cb53742b3106dd3b5
Null McNull <nospam@nowhere.com>
2014-09-16 17:36:05 +0200

    checkbox field

--- src/app/forms/forms.js
+++ src/app/forms/forms.js
@@ -78,7 +78,122 @@ app.factory('forms', function () {
       }
     },
     {
-      name: 'My Second Form'
+      schema: {
+        "fields": [
+          {
+            "type": "text",
+            "name": "field553",
+            "displayName": "Textbox",
+            "validation": {
+              "messages": {}
+            }
+          },
+          {
+            "type": "email",
+            "name": "field555",
+            "displayName": "Email",
+            "validation": {
+              "messages": {}
+            }
+          },
+          {
+            "type": "number",
+            "name": "field557",
+            "validation": {
+              "maxlength": 15,
+              "messages": {}
+            },
+            "displayName": "Number"
+          },
+          {
+            "type": "password",
+            "name": "field559",
+            "displayName": "Password",
+            "validation": {
+              "messages": {}
+            }
+          },
+          {
+            "type": "textarea",
+            "name": "field561",
+            "displayName": "Textarea",
+            "validation": {
+              "messages": {}
+            }
+          },
+          {
+            "type": "checkbox",
+            "name": "field7781",
+            "nolabel": true,
+            "displayName": "Checkbox"
+          },
+          {
+            "type": "checkboxlist",
+            "name": "field7783",
+            "displayName": "Checkbox List",
+            "options": [
+              {
+                "value": "1",
+                "text": "Option 1"
+              },
+              {
+                "value": "2",
+                "text": "Option 2"
+              },
+              {
+                "value": "3",
+                "text": "Option 3"
+              }
+            ],
+            "value": {
+              "1": true,
+              "2": true
+            }
+          },
+          {
+            "type": "radiobuttonlist",
+            "name": "field7787",
+            "displayName": "Radiobutton List",
+            "options": [
+              {
+                "value": "1",
+                "text": "Option 1"
+              },
+              {
+                "value": "2",
+                "text": "Option 2"
+              },
+              {
+                "value": "3",
+                "text": "Option 3"
+              }
+            ],
+            "value": "1"
+          },
+          {
+            "type": "selectlist",
+            "name": "field7789",
+            "displayName": "Select List",
+            "options": [
+              {
+                "value": "1",
+                "text": "Option 1"
+              },
+              {
+                "value": "2",
+                "text": "Option 2"
+              },
+              {
+                "value": "3",
+                "text": "Option 3"
+              }
+            ],
+            "value": "1"
+          }
+        ],
+        "name": "All Fields"
+      },
+      "name": "All Fields"
     }
   ];
 
